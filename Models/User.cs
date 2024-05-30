1fdecdfd8eb3e0d6279c5c7e1340f834a240daa4
Roy Chanunkha <roychanunkha@gmail.com>
2024-05-30 22:09:54 +0200

    fix: user annotations

--- Models/User.cs
+++ Models/User.cs
@@ -1,51 +1,84 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace NaLibApi.Models
 {
-    public class User : NaLibApi.Interfaces.ICreatedUpdatedVoidedEntity
+    public class User : Interfaces.ICreatedUpdatedVoidedEntity
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         [Required(ErrorMessage = "Given name is required")]
-        public string FirstName { get; set; }
-        public required string LastName { get; set; }
-        public required string Email { get; set; }
-        public required string Password { get; set; }
-        public required int? LibraryId { get; set; }
-        public required int CreatedBy { get; set; }
-        public required int? UpdatedBy { get; set; }
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Given name must be between 2 and 50 characters")]
+        public string? FirstName { get; set; }
+        [Required(ErrorMessage = "Family name is required")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Family name must be between 2 and 50 characters")]
+        public string? LastName { get; set; }
+        public string FullName => $"{FirstName} {LastName}";
+        [Required(ErrorMessage = "Email is required")]
+        [StringLength(150, MinimumLength = 6, ErrorMessage = "Email must be between 6 and 100 characters")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string? Email { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(150, MinimumLength = 8, ErrorMessage = "Password must be between 8 and 50 characters")]
+        [JsonIgnore]
+        public string? Password { get; set; }
+        public int? LibraryId { get; set; }
+        public string LibraryName => $"{Library?.Name}";
+        [Required(ErrorMessage = "Creator is required")]
+        [JsonIgnore]
+        public int CreatedBy { get; set; }
+        [JsonIgnore]
+        public int? UpdatedBy { get; set; }
+        [JsonIgnore]
         public bool Voided { get; set; }
+        [JsonIgnore]
         public int? VoidedBy { get; set; }
+        [JsonIgnore]
         public DateTime? VoidedOn { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
 
         // Navigation properties
         [ForeignKey("LibraryId")]
+        [JsonIgnore]
         public Library Library { get; set; }
         
         [ForeignKey("CreatedBy")]
-        public User CreatedByUser { get; set; }
+        [JsonIgnore]
+        public User? CreatedByUser { get; set; }
 
         [ForeignKey("UpdatedBy")]
-        public User UpdatedByUser { get; set; }
+        [JsonIgnore]
+        public User? UpdatedByUser { get; set; }
 
         [ForeignKey("VoidedBy")]
-        public User VoidedByUser { get; set; }
+        [JsonIgnore]
+        public User? VoidedByUser { get; set; }
 
         // Collection navigation properties
-        public ICollection<UserSkill> UserSkills { get; set; }
-        public ICollection<UserRole> UserRoles { get; set; }
-        public ICollection<UserGrade> UserGrades { get; set; }
-        public ICollection<UserExperience> UserExperiences { get; set; }
-        public ICollection<UserContact> UserContacts { get; set; }
-        public ICollection<RoleAccess> RoleAccesses { get; set; }
-        public ICollection<Role> Roles { get; set; }
-        public ICollection<Member> Members { get; set; }
-        public ICollection<MemberContact> MemberContacts { get; set; }
-        public ICollection<MemberRent> MemberRents { get; set; }
-        public ICollection<ContactType> ContactTypes { get; set; }
+        [JsonIgnore]
+        public ICollection<UserSkill>? UserSkills { get; set; }
+        [JsonIgnore]
+        public ICollection<UserRole>? UserRoles { get; set; }
+        [JsonIgnore]
+        public ICollection<UserGrade>? UserGrades { get; set; }
+        [JsonIgnore]
+        public ICollection<UserExperience>? UserExperiences { get; set; }
+        [JsonIgnore]
+        public ICollection<UserContact>? UserContacts { get; set; }
+        [JsonIgnore]
+        public ICollection<RoleAccess>? RoleAccesses { get; set; }
+        [JsonIgnore]
+        public ICollection<Role>? Roles { get; set; }
+        [JsonIgnore]
+        public ICollection<Member>? Members { get; set; }
+        [JsonIgnore]
+        public ICollection<MemberContact>? MemberContacts { get; set; }
+        [JsonIgnore]
+        public ICollection<MemberRent>? MemberRents { get; set; }
+        [JsonIgnore]
+        public ICollection<ContactType>? ContactTypes { get; set; }
     }
 }
